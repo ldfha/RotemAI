97c3a82b911fc679a10a9679666b9847cf70eb5c
ldfha <sowon12020@gmail.com>
2026-02-06 09:14:23 +0900

    2026-02-05 클래스 : 로그변환, 퀴즈 추가

--- projects/pro1/pack1/quiz_class.py
+++ projects/pro1/pack1/quiz_class.py
@@ -3,10 +3,10 @@ class Machine:
     def __init__(self):
         self.coin = int(input('동전을 입력하세요 : '))
         self.cupCount = int(input('몇 잔을 입력하세요 : '))
-        self.coinin = CoinIn()
+        self.coinIn = CoinIn()
         
     def showData(self):
-        success, change = self.coinin.culc(self.coin, self.cupCount)
+        success, change = self.coinIn.culc(self.coin, self.cupCount)
         if success:
             print(f'커피 {self.cupCount}잔과 잔돈 {change}원')
         else: print('요금 부족')
