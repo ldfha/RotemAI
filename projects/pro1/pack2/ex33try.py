f5ad6afe90c80540dc2f8b1c8dcc7e1bd9b7e6a6
ldfha <sowon12020@gmail.com>
2026-02-09 12:02:03 +0900

    2026-02-09 예외처리

---
+++ projects/pro1/pack2/ex33try.py
@@ -0,0 +1,29 @@
+# 예외 처리 : 파일, 네트워크, DB작업, 실행에러 등의 에러 대처
+
+def divide(a, b):
+    return a / b
+
+print('이런 저런 작업 진행...')
+# c = divide(5, 2)
+# c = divide(5, 0)
+# print(c)
+
+try:
+    # 실행문(예외 발생 가능 구문)
+    # c = divide(5, 0)
+    # print(c)
+
+    aa = [1, 2]
+    # print(aa[0])
+    # print(aa[3])
+
+    open("c:/work/abc.txt")
+except ZeroDivisionError:   # 예외 종류 관련 클래스
+    print('두번째 값은 0을 주면 안돼요')    # 예외 발생 처리 구문
+except IndexError as e:
+    print('참조 범위 오류 : ', e)
+except Exception as err:
+    print('에러 : ', err)
+finally:
+    print('에러 유무에 상관없이 반드시 수행됨')
+print('end')
