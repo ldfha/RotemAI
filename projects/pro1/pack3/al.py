c3ab890ac9181fcb35311a8c4ce4407e4c4f4655
ldfha <sowon12020@gmail.com>
2026-03-03 17:58:08 +0900

    2026-03-03 js arrow function, 이벤트 처리, 데이터 유효성 검사, ajax

--- projects/pro1/pack3/al.py
+++ projects/pro1/pack3/al.py
@@ -1,57 +1,16 @@
-'''
-26 5
-Bulbasaur
-Ivysaur
-Venusaur
-Charmander
-Charmeleon
-Charizard
-Squirtle
-Wartortle
-Blastoise
-Caterpie
-Metapod
-Butterfree
-Weedle
-Kakuna
-Beedrill
-Pidgey
-Pidgeotto
-Pidgeot
-Rattata
-Raticate
-Spearow
-Fearow
-Ekans
-Arbok
-Pikachu
-Raichu
-25
-Raichu
-3
-Pidgey
-Kakuna
-'''
-def reverse_dict(dictionary):
-    return dict(map(reversed, dictionary.items()))
+"""
+3 5
+1 2 4
+2 3 4 5 6
+"""
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
-pocketmon={i+1:input().strip() for i in range(n)}
-question=[input().strip() for i in range(m)]
-rpkm = reverse_dict(pocketmon)
-for q in question:
-    if q.isnumeric():
-        print(pocketmon[int(q)])
-    else:
-        print(rpkm[q])
-# jikwon = set()
-# for _ in range(n):
-#     name, status = input().split()
-#     if status == "enter":
-#         jikwon.add(name)
-#     else:
-#         jikwon.remove(name)
-# sorted_jikwon = sorted(jikwon, reverse=True)
-# for j in sorted_jikwon:
-#     print(j)
+
+N, M = map(int, input().split())
+A = set()
+B = set()
+A = set(input().split())
+B = set(input().split())
+
+print(len((A-B) | (B-A)))
+
