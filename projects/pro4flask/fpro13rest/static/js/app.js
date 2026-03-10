e29005d8093ece7d16975519c824c4392472f038
ldfha <sowon12020@gmail.com>
2026-03-10 13:12:55 +0900

    2026-03-10 rest api, ajax, fetch 방법

---
+++ projects/pro4flask/fpro13rest/static/js/app.js
@@ -0,0 +1,42 @@
+// 함수(화살표 함수) 객체 생성 후 $에 할당
+
+const $ = (sel) => document.querySelector(sel);
+// function $(sel){
+//     return document.querySelector(sel);
+// }
+// ex) $("#sendBtn")하면 document.querySelector(sel)가 실행
+
+$("#sendBtn").addEventListener("click", async() =>{
+    // 비동기 처리
+    const name = $("#name").value.trim();
+    // const age = $("#age").value.trim();
+    const age = document.querySelector("#age").value.trim();
+
+    const params = new URLSearchParams({name, age});  // 공백, 한글이 포함된 경우 자동 인코딩 홍길동 -> %ED%78...
+    const url = `/api/friend?${params.toString()}`   // 최종 URL 생성 : /api/friend/name=길동&age=23
+
+    $("#result").textContent = "요청 중..."     // 서버에 자료 요청 시간이 길어지면 보이는 메세지
+
+    try{
+        const rest = await fetch(url, {
+            method:"GET",
+            headers:{"Accept":"application/json"}   // mine type
+        });
+
+        const data = await rest.json(); // 응답 본문을 JSON으로 파싱해서 JS객체화
+
+        if(!rest.ok || data.ok === false){
+            $('#result').innerHTML = `<span class='error'>에러 : ${data.error}</span>`;
+            return;
+        }
+        // 요청 성공인 경우
+         $('#result').innerHTML = `
+         <div>이름 : ${data.name}</div>
+         <div>나이 : ${data.age}</div>
+         <div>연령대 : ${data.age_group}</div>
+         <div>메세지 : ${data.message}</div>
+         `
+    }catch(err){
+        $("#result").innerHTML = `<span class='error'>네트워크, 파싱 오류 : ${err}</span>`;
+    }
+});
