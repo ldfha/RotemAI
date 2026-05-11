4fdde7a48d88a0792eafdc50bedb3d14e6beab7a
ldfha <sowon12020@gmail.com>
2026-05-11 17:38:51 +0900

    2026-05-11 convolution 직접 구현

--- projects/pro12deepltf/tf26before_cnn.py
+++ projects/pro12deepltf/tf26before_cnn.py
@@ -12,6 +12,43 @@ im = rgb2gray(data.coffee())
 im = resize(im, (64, 64))
 print(im.shape)
 
+print(im)
+
 plt.axis('off')
 plt.imshow(im, cmap='gray')
+plt.show()
+
+# 합성곱 필터 ( 3 * 3 )
+filter = np.array([
+    [1, 1, 1],
+    [0, 0, 0],
+    [-1, -1, -1]
+])
+
+# padding : 상하좌우에 1픽셀씩 0으로 채우기
+new_image = np.zeros(im.shape)  # Feature Map
+im_pad = np.pad(im, 1, 'constant')  # 0으로 채우기
+
+# 합성곱(원소별 곱의 합) 연산(Convolution)을 수행
+# 원래 이미지 im의 크기에 대해 모든 픽셀 좌표(i, j)를 훑는다.
+for i in range(im.shape[0]):
+    for j in range(im.shape[1]):
+        try:
+            new_image[i, j] = \
+                im_pad[i - 1, j - 1] * filter[0, 0] + \
+                im_pad[i - 1, j] * filter[0, 1] + \
+                im_pad[i - 1, j + 1] * filter[0, 2] + \
+                im_pad[i, j - 1] * filter[1, 0] + \
+                im_pad[i, j] * filter[1, 1] + \
+                im_pad[i, j + 1] * filter[1, 2] + \
+                im_pad[i + 1, j - 1] * filter[2, 0] + \
+                im_pad[i + 1, j] * filter[2, 1] + \
+                im_pad[i + 1, j + 1] * filter[2, 2]
+        except:
+            pass
+
+print(new_image)
+
+plt.axis('off')
+plt.imshow(new_image, cmap='gray')
 plt.show()
